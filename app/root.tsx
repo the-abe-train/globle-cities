21af0be82ea56346a5d27714027a20e7de5e69ce
the-abe-train <abraham.train@gmail.com>
2022-10-21 14:36:26 -0400

    Added tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,12 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import type { LinksFunction } from "@remix-run/node"; // or cloudflare/deno
+
+import styles from "./tailwind.css";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
